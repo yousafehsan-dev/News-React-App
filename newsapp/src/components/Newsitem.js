fc708639560a76d386f4934002a32cdb81f26018
yousafehsan-dev <yousafehsan125@gmail.com>
2025-11-08 03:17:55 +0500

    change

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -11,12 +11,12 @@ export class Newsitem extends Component {
          <img src={imgurl} className="card-img-top" alt="..."/>
         <div className="card-body">
        <h5 className="card-title">{tittle}</h5>
-      <p className="card-text">{description}</p>
-      <a href={newsurl} target='-blank' className="btn btn-sm btn-primary">Read More</a>
-       </div>
-       </div>
-      </div>
-    )
+       <p className="card-text">{description}</p>
+         <a href={newsurl} target='-blank' className="btn btn-sm btn-dark">Read More</a>
+        </div>
+         </div>
+          </div>
+    )     
   }
 }
 
